1103e23789f76b91085db672db36a59a92229920
Harish Ravichandiran <e18ca015.harish@gmail.com>
2023-12-16 23:59:36 +0530

    Finally test

--- app.js
+++ app.js
@@ -2,26 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const { promisify } = require('util');
+const tmp = require('tmp');
 
 const app = express();
-const port = 3000;
-
-const writeFileAsync = promisify(fs.writeFile);
-const readdirAsync = promisify(fs.readdir);
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 // API endpoint to create a text file with a timestamp
 app.post('/create-file', async (req, res) => {
   try {
-    const folderPath = 'E:\\Filesystem'; // Update with your folder path
+    const tmpDir = tmp.dirSync(); // Create a temporary writable directory
+    const fileName = `${new Date().toISOString().replace(/:/g, '-').slice(0, -5)}.txt`;
+    const filePath = path.join(tmpDir.name, fileName);
 
-    const currentDate = new Date();
-    const fileName = `${currentDate.toISOString().replace(/:/g, '-').slice(0, -5)}.txt`;
-    const filePath = path.join(folderPath, fileName);
+    await fs.promises.writeFile(filePath, new Date().toString());
 
-    await writeFileAsync(filePath, currentDate.toString());
+    // TODO: Move the file to a persistent storage solution (e.g., Amazon S3) here
 
     res.status(201).json({ message: 'Text file created successfully.' });
   } catch (error) {
@@ -33,12 +30,9 @@ app.post('/create-file', async (req, res) => {
 // API endpoint to retrieve all text files in a folder
 app.get('/retrieve-files', async (req, res) => {
   try {
-    const folderPath = 'E://Filesystem'; // Update with your folder path
+    // TODO: Implement retrieval logic based on your storage solution (e.g., Amazon S3)
 
-    const files = await readdirAsync(folderPath);
-    const textFiles = files.filter((file) => file.endsWith('.txt'));
-
-    res.status(200).json({ files: textFiles });
+    res.status(200).json({ files: ['file1.txt', 'file2.txt'] }); // Placeholder response
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
